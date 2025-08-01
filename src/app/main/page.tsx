0aba1d8c45d28137c8a75e1bb83a520b019880ba
Lokesh Venkat <46650625+Lokeshvenkat@users.noreply.github.com>
2025-08-01 05:33:49 +0000

    thanks page

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -3,15 +3,18 @@
 import React, { useState } from "react";
 import Navbar from "@/components/main/Navbar";
 import Menu from "@/components/MenuPage/Menu";
+import ProtectedRoute from "@/components/protectedroute/ProtectedRoute";
 
 const MainPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <div>
-      <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <Menu searchTerm={searchTerm} />
-    </div>
+    <ProtectedRoute>
+      <div>
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <Menu searchTerm={searchTerm} />
+      </div>
+    </ProtectedRoute>
   );
 };
 
---
+++ src/app/thanks/page.tsx
@@ -0,0 +1,9 @@
+import Thanks from "@/components/thanks/Thanks";
+
+export default function ThanksPage() {
+  return (
+    <div>
+      <Thanks />
+    </div>
+  );
+}
